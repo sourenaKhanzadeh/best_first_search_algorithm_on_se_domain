95256bbb9930497cf7fdeb4e9bc6f880d7209601
Sourena Khanzadeh <sourena.khanzadeh@ryerson.ca>
2022-11-08 18:28:58 -0500

    Added: graph simulation
    simulation completed

--- simulate/domain/graphs/simulate_graph.py
+++ simulate/domain/graphs/simulate_graph.py
@@ -3,6 +3,9 @@ import sys
 from pygame.locals import *
 from pygame.color import *
 
+from algorithms.best_first_search.astar import *
+from algorithms.domains.graph_path_finding.graph_path_finding import *
+
 pygame.init()
 
 class Settings:
@@ -103,7 +106,7 @@ class GraphSM:
                     break
             else:
                 if edge.node1 == node2.parent and edge.node2 == node2:
-                    edge.val = 0
+                    edge.val = 1
 
 class Game:
     def __init__(self, settings):
@@ -111,6 +114,11 @@ class Game:
         self.screen = pygame.display.set_mode(self.settings.get_screen_size())
         self.clock = pygame.time.Clock()
         self.graph = GraphSM(self.screen, self.settings)
+        self.nodes = []
+        self.vertices = []
+        self.edges = []
+        self.start_state = None
+        self.goal_state = None
     
     def run(self):
         while True:
@@ -128,21 +136,61 @@ class Game:
                 x = x // self.graph.cell_size
                 y = y // self.graph.cell_size
                 if event.button == 1:
-                    self.graph.add_node(f"{x}, {y}", x, y)
+                    # if x and y are not in the nodes list, add them
+                    if (x, y) not in self.nodes:
+                        self.nodes.append((x, y))
+                        self.graph.add_node(str(len(self.nodes)), x, y)
+                        self.vertices.append(Node(str(len(self.nodes))))
+                        self.vertices[-1].x = x
+                        self.vertices[-1].y = y
+                        if len(self.nodes) == 1:
+                            self.start_state = self.vertices[-1]
+                        elif len(self.nodes) >= 2:
+                            self.goal_state = self.vertices[-1]
+                    else:
+                        # switch to the node
+                        self.graph.nodes[self.nodes.index((x, y))].color = (0, 255, 0)
+            
                 elif event.button == 3:
                     for node in self.graph.nodes:
-                        if node.x == x and node.y == y:
-                            self.graph.add_edge(self.graph.nodes[-1], node)
+                        # if the node is green, add an edge
+                        if node.color == (0, 255, 0):
+                            if (x, y) in self.nodes:
+                                self.graph.add_edge(node, self.graph.nodes[self.nodes.index((x, y))])
+                                # add vertices to the edges
+                                self.edges.append((self.vertices[self.nodes.index((node.x, node.y))], self.vertices[self.nodes.index((x, y))]))
+                            node.color = (255, 0, 0)
+                            break
+
             elif event.type == KEYDOWN:
                 if event.key == K_SPACE:
                     for node in self.graph.trail:
                         for edge in self.graph.edges:
                             if node.parent is None:
                                 break
-                            if edge.node1 == node.parent and edge.node2 == node:
+                            if edge.node1 == node.parent.val and edge.node2 == node.val:
                                 edge.val = 1
                                 self.graph.trail.pop(0)
                                 break
+                elif event.key == K_RETURN:
+                    g = Graph(self.vertices, self.edges)
+                    search_engine = AStar()
+                    search_engine.setTransitionSystem(g.transition_system())
+                    search_engine.setHeuristic(g.heuristic)
+                    search_engine.setCostFunction(g.cost_function)
+                    search_engine.setStartState(self.start_state)
+                    search_engine.setGoalTest(g.goal_test)
+                    g.goal_state = self.goal_state
+                    path = search_engine.search(self.start_state, self.goal_state)
+                    if path:
+                        path  = path[0]
+                        for node in path:
+                            for edge in self.graph.edges:
+                                if node.parent is None:
+                                    break
+                                if edge.node1.text == node.parent.val and edge.node2.text == node.val:
+                                    edge.val = 1
+                                    break
     def _update_screen(self):
         self.screen.fill(self.settings.get_bg_color())
         self.graph.draw()
--- simulate/graph_sim_main.py
+++ simulate/graph_sim_main.py
@@ -12,53 +12,37 @@ from algorithms.best_first_search.astar import *
 from domain.graphs.simulate_graph import *
 
 if __name__ == "__main__":
-    nodes = []
-    positions = [
-        (0, 0),
-        (1, 1),
-        (2, 0),
-        (3, 3),
-        (4, 0),
-        (5, 5),
-    ]
+    
+    # # print(len(vertices))
+    # g = Graph(nodes, vertices)
 
-    for i in range(6):
-        nodes.append(Node(i))
-        # add positions like a tree
+    # g.start_state = nodes[0]
+    # g.goal_state = nodes[5]
 
+    # search_engine = AStar()
+    # # set the transition system
+    # search_engine.setTransitionSystem(g.transition_system())
+    # # set the heuristic
+    # search_engine.setHeuristic(g.heuristic)
+    # # set the cost function
+    # search_engine.setCostFunction(g.cost_function)
 
-    vertices = [(nodes[0], nodes[1]),(nodes[0], nodes[2]),(nodes[1], nodes[4]),(nodes[2], nodes[3]),(nodes[4], nodes[3]),(nodes[3], nodes[5])]
-
-    # print(len(vertices))
-    g = Graph(nodes, vertices)
-
-    g.start_state = nodes[0]
-    g.goal_state = nodes[5]
-
-    search_engine = AStar()
-    # set the transition system
-    search_engine.setTransitionSystem(g.transition_system())
-    # set the heuristic
-    search_engine.setHeuristic(g.heuristic)
-    # set the cost function
-    search_engine.setCostFunction(g.cost_function)
-
-    # set the goal test
-    search_engine.setGoalTest(g.goal_test)
-    # set the start state
-    search_engine.setStartState(g.start_state)
-    # search
-    path, action = search_engine.search(g.start_state, g.goal_state)
+    # # set the goal test
+    # search_engine.setGoalTest(g.goal_test)
+    # # set the start state
+    # search_engine.setStartState(g.start_state)
+    # # search
+    # path, action = search_engine.search(g.start_state, g.goal_state)
 
     game = Game(Settings())
-    for node, pos in zip(nodes, positions):
-        node.x = pos[0]
-        node.y = pos[1]
-        game.graph.add_node(node.val, pos[0], pos[1])
+    # for node, pos in zip(nodes, positions):
+    #     node.x = pos[0]
+    #     node.y = pos[1]
+    #     game.graph.add_node(node.val, pos[0], pos[1])
 
-    for vertex in vertices:
-        game.graph.add_edge(vertex[0], vertex[1])
+    # for vertex in vertices:
+    #     game.graph.add_edge(vertex[0], vertex[1])
     
-    for p in path:
-        game.graph.policy("traverse_edge", p)
+    # for p in path:
+    #     game.graph.policy("traverse_edge", p)
     game.run()
