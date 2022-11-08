ab6cc9b22d9322c92176598e0b49d12400dc2fa2
Sourena Khanzadeh <sourena.khanzadeh@ryerson.ca>
2022-11-08 02:16:44 -0500

    Added a simulation to pathfinding
    Simulation added to path finding

---
+++ simulate/domain/pathfinding/simulate_pathfinding.py
@@ -0,0 +1,185 @@
+import pygame
+from pygame.locals import *
+from pygame.color import *
+import sys
+
+pygame.init()
+
+class Settings:
+    def __init__(self, fps=60):
+        self.screen_width = 800
+        self.screen_height = 600
+        self.bg_color = (255, 255, 255)
+        self.fps = fps
+        self.font = pygame.font.SysFont("Arial", 20)
+        self.font_color = (0, 0, 0)
+    
+    def get_screen_size(self):
+        return (self.screen_width, self.screen_height)
+    
+    def get_bg_color(self):
+        return self.bg_color
+    
+    def get_fps(self):
+        return self.fps
+    
+    def get_font(self):
+        return self.font
+
+    def get_font_color(self):
+        return self.font_color
+
+class Wall:
+    def __init__(self, screen, x, y, cell_size=20):
+        self.screen = screen
+        self.x = x
+        self.y = y
+        self.cell_size = cell_size
+        self.width = self.cell_size
+        self.height =self.cell_size
+    
+    def draw(self):
+        # draw the wall depending on the cell size
+        pygame.draw.rect(self.screen, (0, 0, 0), (self.x * self.cell_size, self.y * self.cell_size, self.width, self.height))
+class Path:
+    def __init__(self, screen, x, y, width, height, cell_size):
+        self.screen = screen
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.trail = []
+        self.temp = []
+        self.cell_size = cell_size
+    
+    def draw(self):
+        for i in self.trail:
+            pygame.draw.rect(self.screen, (0, 0, 255), (i[0], i[1], self.width, self.height))
+        pygame.draw.rect(self.screen, (0, 255, 0), (self.x, self.y, self.width, self.height))
+        
+    def animate(self):
+        if self.trail:
+            self.trail.pop(0)
+        else:
+            self.trail = self.temp[:]
+
+    def policy(self, action):
+        self.trail.append((self.x, self.y))
+        self.temp = self.trail[:]
+        if action.name == 'up':
+            self.y -= self.height
+        elif action.name == 'down':
+            self.y += self.height
+        elif action.name == 'left':
+            self.x -= self.width
+        elif action.name == 'right':
+            self.x += self.width
+
+class GridSM:
+    def __init__(self, settings, screen, walls, cell_size=20, start=None, goal=None):
+        self.settings = settings
+        self.screen = screen
+        self.cell_size = cell_size
+        self.grid_width = self.settings.screen_width // self.cell_size
+        self.grid_height = self.settings.screen_height // self.cell_size
+        self.grid = []
+        self.start = start
+        self.goal = goal
+        self.walls = walls
+        self.start_set = False if start == None else True
+        self.goal_set = False if goal == None else True
+        self.path = Path(self.screen, self.start[0] * self.cell_size, self.start[1] * self.cell_size, self.cell_size, self.cell_size, self.cell_size)
+        self.make_grid()
+    
+    def make_grid(self):
+        for i in range(self.grid_width):
+            self.grid.append([])
+            for j in range(self.grid_height):
+                self.grid[i].append(0)
+    
+    def draw_grid(self):
+        for i in range(self.grid_width):
+            for j in range(self.grid_height):
+                pygame.draw.rect(self.screen, (0, 0, 0), (i * self.cell_size, j * self.cell_size, self.cell_size, self.cell_size), 1)
+    
+    def draw_start(self):
+        if self.start:
+            pygame.draw.rect(self.screen, (0, 255, 0), (self.start[0] * self.cell_size, self.start[1] * self.cell_size, self.cell_size, self.cell_size))
+    
+    def draw_goal(self):
+        if self.goal:
+            pygame.draw.rect(self.screen, (255, 0, 0), (self.goal[0] * self.cell_size, self.goal[1] * self.cell_size, self.cell_size, self.cell_size))
+    
+    def draw_wall(self):
+        for wall in self.walls:
+            wall.draw()
+
+    def draw(self):
+        self.draw_grid()
+        self.draw_start()
+        self.draw_goal()
+        self.draw_wall()
+    
+    def get_cell(self, x, y):
+        return self.grid[x][y]
+    
+    def set_cell(self, x, y, value):
+        self.grid[x][y] = value
+    
+    def set_start(self, x, y):
+        if self.start_set:
+            self.grid[self.start[0]][self.start[1]] = 0
+        self.start = (x, y)
+        self.start_set = True
+        self.grid[x][y] = 1
+    
+    def set_goal(self, x, y):
+        if self.goal_set:
+            self.grid[self.goal[0]][self.goal[1]] = 0
+        self.goal = (x, y)
+        self.goal_set = True
+        self.grid[x][y] = 2
+    
+    def get_start(self):
+        return self.start
+
+    def update(self):
+        self.draw()
+        self.path.draw()
+        self.path.animate()
+
+class Simulate:
+    def __init__(self, settings):
+        self.settings = settings
+        self.screen = pygame.display.set_mode(self.settings.get_screen_size())
+        self.clock = pygame.time.Clock()
+        self.font = self.settings.get_font()
+        self.font_color = self.settings.get_font_color()
+
+        self.scene = []
+
+
+    def awake(self, gameObj):
+        self.scene.append(gameObj)
+
+    def run(self):
+        while True:
+            self.clock.tick(self.settings.get_fps())
+            self.screen.fill(self.settings.get_bg_color())
+            self.handle_events()
+            pygame.display.flip()
+    
+    def handle_events(self):
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+        for gameObj in self.scene:
+            gameObj.update()
+    
+    def __getitem__(self, index):
+        return self.scene[index]
---
+++ simulate/pathfinding_sim_main.py
@@ -0,0 +1,59 @@
+import sys
+import os
+
+# add the parent directory to the path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+# add parent of the parent directory to the path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+
+from domain.pathfinding.simulate_pathfinding import *
+from algorithms.domains.map_path_finding.path_finding import *
+from algorithms.best_first_search.astar import *
+
+
+if __name__ == "__main__":
+    s = Simulate(Settings(fps=30))
+    # create a grid
+    grid = Grid(20, 20)
+    # grid[8, 9].value = 0
+    walls = []
+    # create walls
+    # for i in range(0, 5):
+        # walls.append(Wall(s.screen, i, 0))
+        # walls.append(Wall(s.screen, i, 19))
+        # grid[i, 0].value = 0
+        # grid[i, 19].value = 0
+
+    walls.append(Wall(s.screen, 3, 0))
+    walls.append(Wall(s.screen, 0, 1))
+    walls.append(Wall(s.screen, 0, 2))
+    walls.append(Wall(s.screen, 0, 3))
+    grid[3, 0].value = 0
+    grid[0, 1].value = 0
+    grid[0, 2].value = 0
+    grid[0, 3].value = 0
+
+    s.awake(GridSM(s.settings, s.screen, walls=walls, start=(0, 0), goal=(9, 9)))
+    # create a map
+    map = Map(grid, start=Cell(0, 0, 1), goal=Cell(9, 9, 1))
+    # create a search engine
+    search_engine = AStar(1)
+    # set the transition system
+    search_engine.setTransitionSystem(map.transition_system)
+    # set the heuristic
+    search_engine.setHeuristic(map.heuristic)
+    # set the cost function
+    search_engine.setCostFunction(map.cost_function)
+
+    # set the goal test
+    search_engine.setGoalTest(map.goal_test)
+    # set the start state
+    search_engine.setStartState(map.start_state)
+    # search
+    path, action = search_engine.search(map.start_state, map.goal_state)
+    
+    for a in action:
+        assert type(s.scene[0]) == GridSM, "scene[0] is not a GridSM"
+        s.scene[0].path.policy(a)
+    # print the path
+    s.run()    
